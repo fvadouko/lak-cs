1e11af45468125c945920fbdb9dbd9b7d8efa145
ADOUKO Franck Venance <fvadouko@live.fr>
2020-02-14 11:52:57 +0000

    tak picture with a camra and validate form

--- www/js/index.js
+++ www/js/index.js
@@ -17,30 +17,106 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+  // Application Constructor
+  initialize: function() {
+    document.addEventListener(
+      "deviceready",
+      this.onDeviceReady.bind(this),
+      false
+    );
+    // Disabling form submissions if there are invalid fields
+    this.disableFormSubmission();
+  },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+  // deviceready Event Handler
+  //
+  // Bind any cordova events here. Common events are:
+  // 'pause', 'resume', etc.
+  onDeviceReady: function() {
+    this.receivedEvent("deviceready");
+  },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+  // Update DOM on a Received Event
+  receivedEvent: function() {
+    if (this.hasGetUserMedia()) {
+      console.log("You are all set!");
+      this.takePicture();
+    } else {
+      alert("getUserMedia() is not supported by your browser :/");
     }
+  },
+
+  disableFormSubmission: function() {
+    window.addEventListener(
+      "load",
+      function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.getElementsByClassName("needs-validation");
+        // Loop over them and prevent submission
+        Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener(
+            "submit",
+            function(event) {
+              if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+              }
+              form.classList.add("was-validated");
+            },
+            false
+          );
+        });
+      },
+      false
+    );
+  },
+
+  hasGetUserMedia: function() {
+    return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+  },
+
+  takePicture: function() {
+    const captureVideoButton = document.querySelector("#open-camera");
+    const screenshotButton = document.querySelector("#camera-capture");
+    const img = document.querySelector("#profile-image");
+    const video = document.querySelector("#video-container");
+
+    const canvas = document.createElement("canvas");
+
+    const constraints = {
+      video: { width: { exact: 200 }, height: { exact: 150 } }
+    };
+
+    captureVideoButton.onclick = function() {
+      navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(handleSuccess)
+        .catch(handleError);
+    };
+
+    screenshotButton.onclick = function() {
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      canvas.getContext("2d").drawImage(video, 0, 0);
+      // Other browsers will fall back to image/png
+      img.src = canvas.toDataURL("image/webp");
+      // If the video source Object is set, stop all tracks
+      if (video.srcObject) {
+        video.srcObject.getTracks().forEach(function(track) {
+          track.stop();
+        });
+      }
+    };
+
+    function handleSuccess(stream) {
+      screenshotButton.disabled = false;
+      video.srcObject = stream;
+    }
+
+    function handleError(error) {
+      console.log("Error: ", error);
+    }
+  }
 };
 
 app.initialize();
