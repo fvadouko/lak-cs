0fe57120a2742bb887083846f32e5aebe5730cb2
christian ASSOUKPA <assoukpachristian@yahoo.fr>
2020-03-06 10:57:02 +0000

    Ajout des methodes getPointeusesByUser, getEventsByUser dans l Api:  paiesApi

--- www/js/services/paiesApi.js
+++ www/js/services/paiesApi.js
@@ -72,11 +72,59 @@ async function findAll(year,month){
   }
 
 
+  /*Renvoie un tableau de event
+      {
+        "week": "8",
+        "jour": "5", à faire passer dans un tableau de jour exple [lundi,...]
+        "title": "La sécurité de louer plus simplement",
+        "eventID": "1",
+        "debutPrevu": "2020-02-21 17:30:24",
+        "finPrevu": "2020-02-21 18:00:24",
+        "user": "2"
+    }
+  */
+  async function getEventsByUser(year,month,id){
+    console.log(`year: ${year} - month ${month} - id ${id}`);
+    try {
+        const response = await fetch(`http://localhost:5000/api/paie/getEventsByUser/${year}/${month}/${id}`);
+        const data = await response.json();
+        console.log(data);
+        return data;   
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
+  }
+
+
+  /*Renvoie un tableau de pointeuses
+    {
+        "week": "8",
+        "jour": "5",
+        "pointeusesID": "2",
+        "debutReel": "2020-02-21 17:35:24",
+        "finPrevu": "2020-02-21 18:00:24",
+        "user": "2"
+    },
+  */
+  async function getPointeusesByUser(year,month,id){
+    console.log(`year: ${year} - month ${month} - id ${id}`);
+    try {
+        const response = await fetch(`http://localhost:5000/api/paie/getPointeusesByUser/${year}/${month}/${id}`);
+        const data = await response.json();
+        console.log(data);
+        return data;   
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
+  }
+
   export default {
     findAll,
     getWeeksByUser,
     TotalHoursDone,
     TotalPlanningHours,
     getOvertimes,
-    getUser
+    getUser,
+    getEventsByUser,
+    getPointeusesByUser
   };
