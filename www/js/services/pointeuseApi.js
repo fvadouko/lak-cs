07ff4b3d486208ba863d186b1e6a8d195741ebdb
ADOUKO Franck Venance <fvadouko@live.fr>
2020-03-06 07:53:11 +0000

    jste une correction sur les doublos de fonctions

--- www/js/services/pointeuseApi.js
+++ www/js/services/pointeuseApi.js
@@ -4,58 +4,19 @@ function create(password, arrivals, departures) {
   var myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
 
-  function create(password, arrivals, departures) {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+  var raw = JSON.stringify({
+    passwords: password,
+    arrivals: arrivals,
+    departures: null
+  });
 
-    var raw = JSON.stringify({
-      passwords: password,
-      arrivals: arrivals,
-      departures: null
-    });
+  console.log(raw);
 
-    console.log(raw);
-
-    var requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: raw,
-      redirect: "follow"
-    };
-
-    fetch(config + "api/create/pointeuses", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log("error", error));
-  }
-
-  function update(password, departures) {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-
-    var raw = JSON.stringify({
-      passwords: password,
-      departures: departures
-    });
-
-    console.log(raw);
-
-    var requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: raw,
-      redirect: "follow"
-    };
-
-    fetch(config + "api/edit/pointeuses", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log("error", error));
-  }
-
-  export default {
-    create,
-    update
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow"
   };
 
   fetch(config + "api/create/pointeuses", requestOptions)
